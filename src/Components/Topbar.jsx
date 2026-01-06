c72c7241a86ab65fac938f8b171c1cb40b5b2ea0
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-01-06 11:07:35 +0530

    Changes in Topbar

---
+++ src/Components/Topbar.jsx
@@ -0,0 +1,18 @@
+const Topbar = () => {
+return(
+    <div
+    style={{
+        backgroundColor: '#1B2945',
+        margin : 0,
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        fontSize: '12px',
+    }}>
+        <p>Plot No-26, Dalip Vihar, Chander Vihar, Nilothi Extension, Nangloi, Delhi – 110041
+ </p>
+    </div>
+)
+}
+
+export default Topbar;
