6ef249369c35b4ca13006a386abcfbef80019833
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-01-21 23:08:37 +0530

    Font size in Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,104 +2,94 @@ import {Link} from 'react-router-dom';
 
 const Footer = () => {
     return (
-
-        // Footer Section
-        <div style={{
+       
+        
+        <div
+        style={{
             display: 'flex',
-            width: '100%',
             height: '771.489px',
             padding: '150px 50px 20px 50px',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             gap: '50px',
-            backgroundColor: '#1B2945',
-            color: 'white',
-        }}>
+            backgroundColor: '#1B2945', }}>
 
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'start',
-                justifyContent: 'center',
-            }}>
-        {/* BS International School Logo and Description */}
         <div style={{
-            display: 'flex',
+            paddingTop: 80,
+            flexDirection: 'column',
             justifyContent: 'center',
-            alignItems: 'start',
-            height: '69px',
-            gap: '10px',
+            alignItems: 'center',
+            gap: '50px',
+            alignSelf: 'stretch',
         }}>
-            <img src="./images/logo.png" alt="school logo" style={{
-                width: '69px',
-                aspectRatio: '1/1',
-                borderRadius: '50px'
-            }} />
-
             <div style={{
                 display: 'flex',
-                alignItems: 'center',
-                gap: '10px',
-            }}> 
+                width: '1200px',
+                justifyContent: 'space-between',
+                alignItems: 'flex-start'}}> 
 
-            <h1 style={{  
-                fontFamily: 'Roboto',
-                fontSize: '50px',
-                fontStyle: 'normal',
-                lineHeight: 'short',
-                }}>
-                    BS </h1>
-
-                <p style={{
-                    /* Text Medium - 16 */
-                    fontFamily: 'Roboto',
-                    fontSize: '16px',
-                    fontStyle: 'normal',
-                    fontWeight: 500,
-                    lineHeight: 'normal',
-                }}>International School</p>
-                </div>
-      
-                    </div>
-
-                    {/* About school */}
-                  <p>A school with the right environment goes a long way in shaping the life of a student. 
-                    The great efforts and sacrifices are involved in establishing and maintaining a high quality institution.</p>
-                   
-                </div>
-
-                {/* Footer link Section */}
                 <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    gap: '30px',
+                    alignSelf: 'stretch',
                 }}>
-                    <h1 style={{
-                        borderBottom: "2px solid #F32929"
-                    }}>QUICK LINKS</h1>
-
                     <div style={{
                         display: 'flex',
-                        flexDirection: 'column',
+                        alignItems: 'center',
                         gap: '10px',
                     }}>
-                        <Link to="/" style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                        }}>Home</Link>
-                        <Link to="/about" style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                        }}>About BS International</Link>
-                        <Link to="/facilities" style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                        }}>Facilities</Link>
-                        <Link to="/admission" style={{
-                        textDecoration: 'none',
-                        color: 'white'
-                        }}>Admission</Link>
-                    </div>
+                    <img src="/images/logo.png" alt="Logo" style={{ 
+                        width: '50px', 
+                        height: '50px',
+                        borderRadius: '50px' }} />
+
+                        <h1 style={{
+                            fontSize: '50px',
+                            color: '#FFFFFF',
+                            borderBottom: '2px solid #FFFFFF',
+                        }}>BS</h1>
+
+                        <p style={{
+                            fontSize: '16px',
+                            color: '#FFFFFF'
+                        }}>International School</p>
+
+                        </div>
+
+                        <p style={{
+                            fontSize: '16px',
+                            color: '#FFFFFF',
+                            width: '100%'
+                        }}>A school with the right environment goes a long way in 
+                        shaping the life of a student. The great efforts and 
+                        sacrifices are involved in establishing and maintaining a 
+                        high quality institution.</p>
+
+
                 </div>
+
+                <div style={{
+                    color: '#FFFFFF',
+                }}>
+                        <h1>Quick Links</h1>
+                </div>
+
+                <div>
+
+                </div>
+                            
+            </div>
+
+
+        </div>  
+
         </div>
-    )
-}   
+
+        
+      
+     )
+    }   
 export default Footer;
