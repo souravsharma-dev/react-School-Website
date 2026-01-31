a0b0da84bec08dd1a5c0a3392e552ffd62a95e5d
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-02-01 00:10:42 +0530

    Changes in teachers page

--- src/Components/Teachers.jsx
+++ src/Components/Teachers.jsx
@@ -1,21 +1,62 @@
 import Navbar from "./Navbar";
 
+const teachers = [
+    {
+        image: "./teachers/teacher1.jpg",
+        name: "John Doe",
+        subject: "Mathematics"
+    },
+    {
+        image: "./teachers/teacher2.jpg",
+        name: "Jane Smith",
+        subject: "Science"
+    },
+    {
+        image: "./teachers/teacher3.jpg",
+        name: "Robert Johnson",
+        subject: "History"
+    },
+     {
+        image: "./teachers/teacher4.jpg",
+        name: "Ayush Aggarwal",
+        subject: "Social Science"
+    },
+     {
+        image: "./teachers/teacher5.jpg",
+        name: "Roshni Roy",
+        subject: "English"
+    }
+]
+
 const Teachers = () => {
     return (
 
         <div>
             <Navbar />
+            
 
-            <section>
-                <div style={
-                    {
-                        Gap: "60px"
-                    }
-                }>
-                    <img src="teacher-image.jpg" alt="Teacher Image" />
-                    <h1>Teachers Name</h1>
-                    <p>Subject: Mathematics</p>
-                </div>
+
+            <section
+            style={{
+                width: "80%",
+                padding: '50px',
+                display: 'flex',
+                gap: '20px'
+            }}>
+                {
+                    teachers.map(function (item){ 
+                        return (
+                            <div style={{
+                                width: "20%",
+                            }}>
+                           <img src={item.image} alt="Teacher Image" />
+                           <h1>{item.name}</h1>
+                           <p>{item.subject}</p>
+                           </div>
+                        )
+                    })
+                }
+
             </section>
         </div>
     )
