00e2e3dd76e93730e02048755d1e4a48f3c81d89
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-02-04 23:52:10 +0530

    Changes in the UI

--- src/Components/Teachers.jsx
+++ src/Components/Teachers.jsx
@@ -59,7 +59,7 @@ const Teachers = () => {
                            <h1>{item.name}</h1>
                            <p style={{
                             color: 'white',
-                            background: 'black'
+                            background: 'orange'
                             
                            }}>{item.subject} </p>
                            </div>
