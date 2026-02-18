42499b536d795af0a368b93b17a890ae9a0e814a
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-02-18 22:58:22 +0530

    change in content

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
             {data ? "Hide Data" : "Show Data"}
            </button>
            <div>
-            <p>BS International School believes that education is not just about academics, but about shaping confident, responsible, and compassionate individuals. We provide a balanced learning environment where students are encouraged to explore, question, and grow.
+            <p>BS Internatironal School believes that education is not just about academics, but about shaping confident, responsible, and compassionate individuals. We provide a balanced learning environment where students are encouraged to explore, question, and grow.
 
                 With a strong foundation in values and modern teaching practices, we strive to create lifelong learners prepared for future challenges.</p>
           </div>
