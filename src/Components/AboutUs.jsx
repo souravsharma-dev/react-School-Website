f20777a6bfdb7dd87f884277f56d3bb3dba7f348
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-03-29 19:39:34 +0530

    change in content

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
         <div>
             <Navbar />
             <div>
-                <h1>About Us Component React XOXOX</h1>
+                <h1>About Us Component React XOOX</h1>
             </div>
         </div>
     )   
