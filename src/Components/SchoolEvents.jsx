a0998bf08d8f7d9de80466c676df62e1d765990a
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-04-07 22:09:43 +0530

    change in UI

--- src/Components/SchoolEvents.jsx
+++ src/Components/SchoolEvents.jsx
@@ -1,10 +1,83 @@
 import Navbar from "./Navbar";
 
 const SchoolEvents = () =>{
+
+    const events = [
+        {
+            name: 'Science Fair',
+            date: '2024-07-15',
+            time: '10:00 AM - 2:00 PM'
+        },
+        {
+            name: 'Maths Fair',
+            date: '2024-07-15',
+            time: '10:00 AM - 2:00 PM'
+        },
+        {
+            name: 'SocialScience Fair',
+            date: '2024-07-15',
+            time: '10:00 AM - 2:00 PM'
+        },
+        {
+            name: 'English Fair',
+            date: '2024-07-15',
+            time: '10:00 AM - 2:00 PM'
+        },
+    ]
+
     return (
         <div>
             <Navbar />
-            <h1>School Events Component</h1>
+
+            <div
+            style={{
+                width: '60%',
+                margin: '0 auto',
+                padding: '64px 0'
+            }}>
+            <h1 style={{
+                textAlign:'center'
+
+            }}>Upcoming School Events Component</h1>
+
+            <table width= "100%">
+                <thead>
+                    <tr style={{
+                        background: '#ddd',
+                        padding: '40'
+                    }}>
+                        <th>Sr No.</th>
+                    <th style={{padding:8}}>Event</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        events.map((event, index) => (
+                            <tr style={{
+                                textAlign: "center"
+                            }}>
+                                <td style={
+                                    {
+                                        padding: 8, border: '1px solid #ccc'
+                                    }}>{index+1}</td>
+                                <td style={{
+                                    padding: 8, border: '1px solid #ccc'
+                                }}>{event.name}</td>
+                                <td style={{
+                                    padding: 8, border: '1px solid #ccc'
+                                }}>{event.date}</td>
+                                <td style={{
+                                    padding: 8, border: '1px solid #ccc'
+                                }}>{event.time}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+            </div>
+
         </div>
     )
 }
